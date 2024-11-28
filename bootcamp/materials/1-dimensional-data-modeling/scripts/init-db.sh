6249db43059cbdc21a01f4b25ef836df13895722
KHALED MOHAMED FATHALLAH MOHAMED <THEKINGSTAR5002@GMAIL.COM>
2024-11-28 04:52:07 +0200

    make my posgers work on windows

--- bootcamp/materials/1-dimensional-data-modeling/scripts/init-db.sh
+++ bootcamp/materials/1-dimensional-data-modeling/scripts/init-db.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
 set -e
 
-# Restore the dump file using pg_restore
-pg_restore \
-    -v \
-    --no-owner \
-    --no-privileges \
-    -U $POSTGRES_USER \
-    -d $POSTGRES_DB \
-    /docker-entrypoint-initdb.d/data.dump
+echo "Checking if database exists..."
+DB_EXISTS=$(psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -tAc "SELECT 1 FROM pg_database WHERE datname = '$POSTGRES_DB'")
 
-# Check if the path is a directory using the -d flag and
-#  there are SQL files in the directory using the -f command
-#   (the [] brackets are used for conditional expressions)
-if [ -d /docker-entrypoint-initdb.d/homework ]; then
-  echo "[SUCCESS]: Located homework directory"
-  # Run any additional initialization scripts
-    for f in /docker-entrypoint-initdb.d/homework/*.sql; do
-      if [ -f "$f" ]; then
-        echo "[SUCCESS] Running SQL file: $f"
-        psql -U $POSTGRES_USER -d $POSTGRES_DB -f $f
-      else
-        echo "[INFO] No SQL file found inside the homework directory"
-      fi
-    done
+if [ "$DB_EXISTS" != "1" ]; then
+  echo "Database does not exist. Restoring from data.dump..."
+  pg_restore --clean --no-owner -U "$POSTGRES_USER" -d "$POSTGRES_DB" /docker-entrypoint-initdb.d/data.dump
 else
-    echo "[ERROR] Directory not found: /docker-entrypoint-initdb.d/homework/"
+  echo "Database already exists. Skipping restore."
 fi
